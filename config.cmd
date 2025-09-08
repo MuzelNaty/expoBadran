a3edb17837c1791b46bc6cb4e7d25944e2907b71
MuzelNaty <muzel.naty@gmail.com>
2025-09-08 08:55:37 -0300

    teste

--- config.cmd
+++ config.cmd
@@ -1,4 +1,2 @@
-git config user.name "eloise2801"
-git config user.email "diaseloise77@gmail.com"
-git config user.name "ErickSSebastiao"
-git config user.email "erick322256l@gmail.com"
+git config user.name "MuzelNaty"
+git config user.email "muzel.naty@gmail.com"
